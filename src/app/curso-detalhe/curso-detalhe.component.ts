27a606aca1b6b73e1bc2f8d753873e8ae3d0f16c
amandaxm <amandaxaviermariano10@gmail.com>
2020-09-29 12:20:00 -0300

    Escutando mudanças nos parâmetros de roteamento

--- src/app/curso-detalhe/curso-detalhe.component.ts
+++ src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-curso-detalhe',
@@ -8,11 +9,19 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CursoDetalheComponent implements OnInit {
   id: string;
+  inscrição: Subscription;
   constructor(private route: ActivatedRoute) {
-    this.id = this.route.snapshot.params['id'];
+  //  this.id = this.route.snapshot.params['id'];
    }
 
   ngOnInit(): void {
+   this.inscrição= this.route.params.subscribe(
+     (params: any)=>{
+       this.id =params['id'];
+     }
+   );//inscrever nas mudancas do parametro
   }
-
+  ngOnDestroy(): void {
+    this.inscrição.unsubscribe();
+  }
 }
