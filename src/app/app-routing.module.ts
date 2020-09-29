65e309fc72fde1a3354403edfd1d7e6ba294f230
amandaxm <amandaxaviermariano10@gmail.com>
2020-09-29 12:09:30 -0300

    Definindo parâmetros de roteamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+
+const appRoutes: Routes = [
+  { path: 'cursos', component: CursosComponent },
+  { path: 'curso/:id', component: CursoDetalheComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent }
+    ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(appRoutes, {useHash: true})],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
