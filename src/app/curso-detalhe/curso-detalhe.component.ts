5e8423b3060e6b3c6e1971fdc76be39d0b3b7a93
amandaxm <amandaxaviermariano10@gmail.com>
2020-09-30 09:45:56 -0300

    Redirecionando via código

--- src/app/curso-detalhe/curso-detalhe.component.ts
+++ src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { CursosService } from './../cursos/cursos.service';
+
 
 @Component({
   selector: 'app-curso-detalhe',
@@ -8,20 +10,36 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./curso-detalhe.component.css']
 })
 export class CursoDetalheComponent implements OnInit {
-  id: string;
-  inscrição: Subscription;
-  constructor(private route: ActivatedRoute) {
-  //  this.id = this.route.snapshot.params['id'];
-   }
 
-  ngOnInit(): void {
-   this.inscrição= this.route.params.subscribe(
-     (params: any)=>{
-       this.id =params['id'];
-     }
-   );//inscrever nas mudancas do parametro
+  id: number;
+  inscricao: Subscription;
+  curso: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private cursosService: CursosService,
+    private router: Router,
+  ) { 
+    
   }
-  ngOnDestroy(): void {
-    this.inscrição.unsubscribe();
+
+  ngOnInit() {
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        this.id = params['id'];
+
+        this.curso = this.cursosService.getCurso(this.id);
+
+        if(this.curso ==  null){
+          this.router.navigate(['/cursos/naoEncontrado']);
+        }
+
+      }
+    );
   }
+
+  ngOnDestroy(){
+    this.inscricao.unsubscribe();
+  }
+
 }
